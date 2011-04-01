81158147dc3645464879d51f736697a26e8e2db1
ning@nwang.info <ning@nwang.info@8d389d3a-4d56-1e4e-e551-129e3f19a01d>
2011-04-01 13:17:27 +0000

    add work scenario

--- script/runall.sh
+++ script/runall.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 #echo "Nightly Run Successful: $(date)" >> /media/bak/testbed/script/tmp/mybackup.log
-#sce.sh 4 
-#sce.sh 10
-# sce.sh 16
 for((i = 1; $i <= 18; i++)); do
   time startsce.sh /media/temp/work1/$i/scenario.xml
 done
